f21d74c1fb8bda8f4d329688f5a3e07a6cafdbdd
Serrano, Fernando <fernando.serrano@cbsinteractive.com>
2018-02-03 21:38:57 -0500

    added some views still needs urls for app and needs front end using jinja

--- SMU/SMU/settings.py
+++ SMU/SMU/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SMU.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['SMU/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- SMU/main/views.py
+++ SMU/main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 
-# Create your views here.
+from django.views.generic.edit import FormView
+from django.views.generic import ListView
+
+from django.shortcuts import render
+from main.models import *
+
+
+class Home(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
+
+    def get(self, request):
+        queryset = User.objects.all()
+        return Response({'user': queryset})
+        
+class Profile(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'pages/profile.html'
+
+    def get(self, request):
+        user = User.objects.all()
+        return Response({'user': user})
